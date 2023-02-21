a2924fed9dcae82ba4126d96f292dc0964ff935d
yanez <yanez@DESKTOP-5EGP0FU>
2023-02-20 21:01:19 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage() 
+        {
+            InitializeComponent();
+        }   
+         
+        private async void btnsuma_Clicked(object sender, EventArgs e) 
+        {
+            var numberOne = Double.Parse(numerouno.Text);
+            var numberTwo = Double.Parse(numerodos.Text);
+            var result = numberOne + numberTwo;      
+            await Navigation.PushAsync(new Views.Result(result));
+        }
+           
+        private async void btnresta_Clicked(object sender, EventArgs e)
+        {
+            var numberOne = Double.Parse(numerouno.Text);
+            var numberTwo = Double.Parse(numerodos.Text);     
+                
+            var result = numberOne - numberTwo;
+            await Navigation.PushAsync(new Views.Result(result));
+        }   
+
+        private async void btnmultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            var numberOne = Double.Parse(numerouno.Text);
+            var numberTwo = Double.Parse(numerodos.Text);
+            var result = numberOne * numberTwo;
+            await Navigation.PushAsync(new Views.Result(result));
+        }
+
+        private async void btndivicion_Clicked(object sender, EventArgs e)
+        {
+            var numberOne = Double.Parse(numerouno.Text);
+            var numberTwo = Double.Parse(numerodos.Text);
+            var result = numberOne / numberTwo;
+            await Navigation.PushAsync(new Views.Result(result));
+        }
+    }
+}
